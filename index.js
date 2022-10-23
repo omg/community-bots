3d0a60c4b2cb2a1d1cc0b7a97e0727e72a43c571
headpatmonster <krysppy@gmail.com>
2022-10-23 08:16:37 -0500

    Add tests

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ require('dotenv').config();
 
 //require('./discord.js');
 const { getCurrentRoundInfo } = require('./db.js');
+const { setPresence, sendImportantMessageThatNeedsToBeReceived } = require('./discord.js');
 
 let streak;
 let lastWinner;
 
 async function main() {
   ({ streak, lastWinner } = await getCurrentRoundInfo());
-  
+  sendImportantMessageThatNeedsToBeReceived("813091079322599425", "hey");
+  console.log("test")
+  await sendImportantMessageThatNeedsToBeReceived("813091079322599425", "yo");
+  console.log("test 2");
 }
+
+setPresence("doodoo", true);
 
 // getCurrentRoundInfo().then((info) => {
 //   console.log(info);
@@ -18,3 +24,7 @@ async function main() {
 // getDatabase().then((db) => {
   
 // });
+
+main().then(() => {
+  console.log("Done!");
+})
