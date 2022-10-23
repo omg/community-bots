fa0521a79d5de11c43a102a1c3456d6a5843e815
kgpdx <krysppy@gmail.com>
2022-10-22 21:36:43 -0500

    Start game code

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 require('dotenv').config();
 
-require('./db.js');
-require('./discord.js');
+//require('./discord.js');
+const { getCurrentRoundInfo } = require('./db.js');
+
+let streak;
+let lastWinner;
+
+async function main() {
+  ({ streak, lastWinner } = await getCurrentRoundInfo());
+  
+}
+
+// getCurrentRoundInfo().then((info) => {
+//   console.log(info);
+// });
+
+// getDatabase().then((db) => {
+  
+// });
