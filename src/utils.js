91c275ef797563858138e7caa0832368760deda4
headpatmonster <krysppy@gmail.com>
2022-11-02 16:58:30 -0500

    More utils for remarks

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,29 @@ function formatNumber(x) {
   return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
 
+function formatPercentage(x) {
+  return parseFloat(x * 100).toFixed(1) + '%';
+}
+
+function formatPlacement(x) {
+  let checker = BigInt(x) % 100n;
+  if (checker > 10 && checker < 20) return x + 'th';
+  checker %= 10n;
+  if (checker == 1) return formatNumber(x) + 'st';
+  if (checker == 2) return formatNumber(x) + 'nd';
+  if (checker == 3) return formatNumber(x) + 'rd';
+  return formatNumber(x) + 'th';
+}
+
+function createEnglishList(items) {
+  if (items.length === 0) return '';
+  if (items.length === 1) return items[0];
+  if (items.length === 2) return items[0] + ' and ' + items[1];
+  let listString = items[0];
+  for (let i = 1; i < items.length; i++) listString += (i == items.length - 1 ? ', and ' : ', ') + items[i];
+  return listString;
+}
+
 function shuffle(array) {
   let currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -22,11 +45,15 @@ function shuffle(array) {
 }
 
 function escapeDiscordMarkdown(string) {
+  // \*_\\|~>`<@#!:\. 
   return string.replace(/[\\`_~\*\|]/g, '\\$&');
 }
 
 module.exports = {
   formatNumber,
   shuffle,
-  escapeDiscordMarkdown
+  escapeDiscordMarkdown,
+  formatPercentage,
+  formatPlacement,
+  createEnglishList
 }
