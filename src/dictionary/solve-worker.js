45167e67966b28aac5376eaa84447cdb8443412e
headpatmonster <krysppy@gmail.com>
2022-11-02 16:58:43 -0500

    Add some (useless) helpful comments

--- src/dictionary/solve-worker.js
+++ src/dictionary/solve-worker.js
@@ -2,6 +2,11 @@ process.on('message', (data) => {
   const { dictionaryString, regexSource } = data;
   const promptRegex = new RegExp(regexSource, "gm");
 
+  // /^.*(\R).*$/gm
+
+  // console.log("Solving prompt with regex: " + promptRegex);
+  // console.log("Source: " + promptRegex.source);
+
   let solutions = [];
 
   let match;
