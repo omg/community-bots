abfd635e28ea31550da2f5352609ccc6cab72692
66minmin <krysppy@gmail.com>
2023-05-03 17:44:24 -0500

    Start making a game state

--- lame-bot/games/WordBombMini.ts
+++ lame-bot/games/WordBombMini.ts
@@ -1,5 +1,18 @@
 import Game from "./classes/LameGame";
 
+type WordBombMiniState = {
+  guild: string; // maybe
+  channel: string; // maybe
+  streak: number;
+  lastWinner: number;
+  startedAt: Date; // timestamp really
+  prompt: RegExp; // i think it's regexp but idk
+  promptWord: string;
+  solutions: number;
+  lengthRequired: boolean;
+  topRemarks;
+}
+
 export default class WordBombMini extends Game {
   constructor() {
     super();
