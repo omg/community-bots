3e515b767f50ef87d1f5fa690e602bf7182ebef2
iminlikewithyou <christian@omg.games>
2023-08-26 12:21:06 -0500

    Create new permission getters

--- src/permissions.ts
+++ src/permissions.ts
@@ -6,6 +6,23 @@ import { ApplicationCommandPermissionType, ApplicationCommandPermissions, Client
 
 const guildID = process.env.GUILD_ID;
 
+export function getChannelIDsInCategoryID(client: Client, category: string): string[] {
+  let guild = client.guilds.cache.get(process.env.GUILD_ID);
+  let channels = guild.channels.cache.filter((channel: GuildBasedChannel) => channel.isTextBased() && channel.parentId === category);
+  return channels.map(c => c.id);
+}
+
+export function getChannelIDsInCategoryName(client: Client, category: string): string[] {
+  let guild = client.guilds.cache.get(process.env.GUILD_ID);
+  let channels = guild.channels.cache.filter((channel: GuildBasedChannel) => channel.isTextBased() && channel.parent?.name === category);
+  return channels.map(c => c.id);
+}
+
+export function getChannelIDsByChannelName(client: Client, name: string): string[] {
+  let guild = client.guilds.cache.get(process.env.GUILD_ID);
+  let channels = guild.channels.cache.filter((channel: GuildBasedChannel) => channel.isTextBased() && channel.name === name);
+  return channels.map(c => c.id);
+}
 
 export function allChannels(permission: boolean): ApplicationCommandPermissions {
   return {
@@ -34,6 +51,18 @@ export function channel(channelID: string, permission: boolean): ApplicationComm
 export function role(roleID: string, permission: boolean): ApplicationCommandPermissions {
   return {
     id: roleID,
+    type: ApplicationCommandPermissionType.Role,
+    permission: permission
+  }
+}
+
+export function roleFromName(client: Client, roleName: string, permission: boolean): ApplicationCommandPermissions {
+  let guild = client.guilds.cache.get(process.env.GUILD_ID);
+  let role = guild.roles.cache.find(r => r.name === roleName);
+  if (!role) return null;
+
+  return {
+    id: role.id,
     type: ApplicationCommandPermissionType.Role,
     permission: permission
   }
