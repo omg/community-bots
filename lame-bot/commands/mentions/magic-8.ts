3bf170033b99c904e79e8c617de2f6d316fe86c0
66minmin <christian@omg.games>
2023-10-15 01:07:21 -0500

    create roll and magic8 mention commands

---
+++ lame-bot/commands/mentions/magic-8.ts
@@ -0,0 +1,51 @@
+import { Message } from "discord.js";
+import * as magic8 from "../magic-8";
+
+export const NAME_DUMB = "magic-8"
+
+export const cooldown = magic8.cooldown;
+export const type = magic8.type;
+
+export const limits = magic8.limits;
+
+export const index = 25;
+
+const questions = [
+  // /^wh?a?t /i,
+  /^will? /i,
+  /^wo?n'?t /i,
+  /^isn?'?t? /i,
+  /^(?:are?|r)n?'?t? /i,
+  /^am /i,
+  /^doe?sn?'?t? /i,
+  /^don?'?t?/i,
+  /^can /i,
+  /^co?u?l?dn?'?t? /i,
+  /^sho?u?ldn?'?t? /i,
+  /^wo?u?l?dn?'?t? /i,
+  /^ha(?:ve|s)n?'?t? /i,
+  /^didn?'?t? /i,
+  /^may /i,
+  /^mig?htn?'?t? /i,
+  /^mu?stn?'?t? /i,
+  /^sha?ll?n?'?t? /i,
+];
+
+export function matches(text: string) {
+  for (let i = 0; i < questions.length; i++) {
+    if (questions[i].test(text)) return true;
+  }
+  return false;
+}
+
+export async function execute(message: Message, text: string) {
+  let response = await message.reply({
+    content: "https://omg.games/assets/rolling.gif"
+  });
+
+  setTimeout(async () => {
+    await response.edit({
+      content: magic8.getResponse()
+    });
+  }, 1200);
+}
