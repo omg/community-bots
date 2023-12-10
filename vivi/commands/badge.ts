fd1fb47751c97c0e72d42a5074415570002c25d9
iminlikewithyou <christian@omg.games>
2023-12-10 17:11:35 -0600

    add boosterPositionRole id

--- vivi/commands/badge.ts
+++ vivi/commands/badge.ts
@@ -17,7 +17,7 @@ export const data = new SlashCommandBuilder()
 
 export const broadcastable = false;
 
-const boosterPositionRole = "";
+const boosterPositionRole = "1183517686232268904";
 const boosterRole = "638907551215255553";
 
 const validateUserName = (name: string, _id?: string) => {
