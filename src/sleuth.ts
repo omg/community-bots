12fd6d95a912d9d43cd0754a6350e4826b2a3530
66minmin <christian@omg.games>
2023-12-11 16:24:39 -0600

    not a bugfix just a debug i'm sending to production : - D

--- src/sleuth.ts
+++ src/sleuth.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, ActivityType, Role } from 'discord.js';
+import { Client, GatewayIntentBits, ActivityType, Role, RoleResolvable } from 'discord.js';
 import path from 'node:path';
 
 export const sleuthClient = new Client({
@@ -45,10 +45,12 @@ export async function createBoosterIcon(name: string, rolePos: number, iconResiz
 export async function assignRole(userID: string, roleID: string): Promise<void> {
   let guild = sleuthClient.guilds.cache.get(process.env.GUILD_ID);
 
-  let role = guild.roles.cache.get(roleID);
   let member = guild.members.cache.get(userID);
 
-  await member.roles.add(role);
+  console.log(userID, roleID);
+  console.log(member);
+
+  await member.roles.add(roleID);
 }
 
 export async function setRoleIcon(roleID: string, icon: Buffer) {
