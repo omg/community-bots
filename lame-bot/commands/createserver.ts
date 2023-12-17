04aaff8f24c180a625059230322cdf0921cbb678
66minmin <christian@omg.games>
2023-12-17 04:21:22 -0600

    import crypto

--- lame-bot/commands/createserver.ts
+++ lame-bot/commands/createserver.ts
@@ -1,5 +1,6 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 import { getInteractionContent, replyToInteraction } from "../../src/command-handler";
+import crypto from "crypto";
 
 export const data = new SlashCommandBuilder()
   .setName("createserver")
