36ff3d227fce396d98f678b012275ba6d4888130
66minmin <christian@omg.games>
2024-01-12 17:29:20 -0600

    add mega comments to index.ts

--- core/index.ts
+++ core/index.ts
@@ -1,2 +1,35 @@
-import availableModules from './availableModules';
+import availableModules from "./availableModules";
 
+// if we haven't even set up singleton mode or community mode, we need to do that first
+
+import "./config";
+
+// get the available modules
+
+// if we're in community mode:
+// see if we're missing any modules that we haven't set up yet
+// go through the flow of setting up the module
+
+// if we're in singleton mode:
+// ask if we want to enable the new modules
+
+// is it possible to check the permissions we have of the OAuth token?
+// if we don't have the correct permissions, we shouldn't start up Community Bots until we change the scope
+
+// start each module up
+
+
+
+
+// additional functionality from the terminal
+// (all of these functions have already been implemented in config.ts):
+
+// change between singleton and community mode
+// enable or disable modules
+// change the singleton bot
+// add new bots
+// change bots of modules
+
+// not implemented:
+
+// join a bot to an additional guild after setup (or rejoin them if they were removed)
