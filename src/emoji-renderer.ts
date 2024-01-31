67fd32ae242c33d7325e521032273be310443c6a
iminlikewithyou <iilwy@omg.games>
2024-01-31 06:31:34 -0600

    fix emoji renderer bug with new regex system

--- src/emoji-renderer.ts
+++ src/emoji-renderer.ts
@@ -38,6 +38,8 @@ export function getEmojiFromMap(emoji: string, emojiMap: EmojiMap): string {
 
 /**
  * Replaces every letter in the given string with an emoji from the provided letter map.
+ * 
+ * The letters in the string are converted to uppercase before being replaced.
  *
  * @param string The input string
  * @param letterMap A map of letters to emojis
@@ -46,6 +48,7 @@ export function getEmojiFromMap(emoji: string, emojiMap: EmojiMap): string {
 export function replaceTextWithLetterMap(string: string, letterMap: EmojiMap): string {
   // replace every letter in the string with an emoji from the letterMap
   return string
+    .toUpperCase()
     .split("")
     .map((letter) => {
       return getEmojiFromMap(letter, letterMap);
