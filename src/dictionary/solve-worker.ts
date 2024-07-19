1ecd8db628b4244013abda1547fdb2a755fb40d6
iminlikewithyou <christian@omg.games>
2024-07-19 17:20:07 -0500

    remove solving console logs

--- src/dictionary/solve-worker.ts
+++ src/dictionary/solve-worker.ts
@@ -1,19 +1,15 @@
 process.on("message", (data: { dictionary: string[], regex: string }) => {
-  const start = Date.now();
 
   let { regex } = data;
   let newRegex = new RegExp(regex, "i");
   let solutions = [];
-  console.time("Solving");
+  // console.time("Solving");
   for (let word of data.dictionary) {
     if (newRegex.test(word)) {
       solutions.push(word);
     }
   };
-  console.timeEnd("Solving");
-
-  const end = Date.now();
-  console.log("Took " + (end - start) + "ms to solve.");
+  // console.timeEnd("Solving");
   
   process.send(solutions);
 });
