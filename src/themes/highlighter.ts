1721dab6f54d97115edcd1455e2268f00bc8ac89
iminlikewithyou <christian@omg.games>
2024-07-28 15:54:19 -0500

    rewrite highlighter logic

--- src/themes/highlighter.ts
+++ src/themes/highlighter.ts
@@ -1,15 +1,69 @@
-import { EmojiMap, replaceTextWithLetterMap } from "../emoji-renderer";
+import { CharacterMap, CharacterTheme } from "../highlighting/CharacterSet";
+import CaffeinatedCharacterTheme from "../highlighting/highlighters/Caffeinated";
+import ComicSansCharacterTheme from "../highlighting/highlighters/ComicSans";
+import DarkCharacterTheme from "../highlighting/highlighters/Dark";
+import DefaultCharacterTheme from "../highlighting/highlighters/Default";
+import GreenCharacterTheme from "../highlighting/highlighters/Green";
+import LifeboatCharacterTheme from "../highlighting/highlighters/Lifeboat";
+import LilacCharacterTheme from "../highlighting/highlighters/Lilac";
+import RoseCharacterTheme from "../highlighting/highlighters/Rose";
+import TextCharacterTheme from "../highlighting/highlighters/Text";
+import ViviCharacterTheme from "../highlighting/highlighters/Vivi";
+import { getHighlightedLetters, setHighlightGroups } from "../regex";
 
-interface HighlighterOptions {
-  presentEmojiMap: EmojiMap;
-  highlightedEmojiMap: EmojiMap;
-  wildcardEmojiMap: EmojiMap;
-}
+export class Highlighter {
+  private theme: CharacterTheme;
 
-export abstract class Highlighter {
-  abstract getPresent(string: string): string;
-  abstract getHighlighted(string: string): string;
-  abstract getWildcard(string: string): string;
+  constructor(theme: CharacterTheme) {
+    this.theme = theme;
+  }
+
+  /**
+   * Maps a string to a new string using the provided map.
+   * 
+   * @param string The string to map
+   * @param map The map to use when converting the string
+   * @returns The mapped string
+   */
+  private mapText(string: string, map: CharacterMap) {
+    return string
+      .toUpperCase()
+      .split("")
+      .map((letter) => {
+        return map[letter] ?? map.unknown;
+      })
+      .join("");
+  }
+
+  /**
+   * Applies the present highlight to a string.
+   * 
+   * @param string The string to get the present highlight for
+   * @returns The string with the present highlight applied
+   */
+  getPresent(string: string): string {
+    return this.mapText(string, this.theme.Present);
+  }
+
+  /**
+   * Applies the highlighted highlight to a string.
+   * 
+   * @param string The string to get the highlighted highlight for
+   * @returns The string with the highlighted highlight applied
+   */
+  getHighlighted(string: string): string {
+    return this.mapText(string, this.theme.Highlighted);
+  }
+
+  /**
+   * Applies the wildcard highlight to a string.
+   * 
+   * @param string The string to get the wildcard highlight for
+   * @returns The string with the wildcard highlight
+   */
+  getWildcard(string: string): string {
+    return this.mapText(string, this.theme.Wildcard);
+  }
 
   /**
    * Converts a string to one with the letters converted into its highlighted form.
@@ -42,72 +96,20 @@ export abstract class Highlighter {
   }
 }
 
-export class EmojiHighlighter extends Highlighter implements HighlighterOptions {
-  presentEmojiMap: EmojiMap;
-  highlightedEmojiMap: EmojiMap;
-  wildcardEmojiMap: EmojiMap;
-
-  constructor(options: HighlighterOptions) {
-    super();
-    this.presentEmojiMap = options.presentEmojiMap;
-    this.highlightedEmojiMap = options.highlightedEmojiMap;
-    this.wildcardEmojiMap = options.wildcardEmojiMap;
-  }
-
-  getPresent(string: string): string {
-    return replaceTextWithLetterMap(string, this.presentEmojiMap);
-  }
-
-  getHighlighted(string: string): string {
-    return replaceTextWithLetterMap(string, this.highlightedEmojiMap);
-  }
-
-  getWildcard(string: string): string {
-    return replaceTextWithLetterMap(string, this.wildcardEmojiMap);
-  }
-}
-
-class TextHighlighter extends Highlighter {
-  getPresent(string: string): string {
-    return string;
-  }
-
-  getHighlighted(string: string): string {
-    return `**${string}**`;
-  }
-
-  getWildcard(string: string): string {
-    return this.getHighlighted(string);
-  }
-}
-
-import defaultPresentEmojiMap from "../../assets/emoji-maps/highlighters/default/present";
-import defaultHighlightedEmojiMap from "../../assets/emoji-maps/highlighters/default/highlighted";
-import defaultWildcardEmojiMap from "../../assets/emoji-maps/highlighters/default/wildcard";
-const DefaultHighlighter = new EmojiHighlighter({
-  presentEmojiMap: defaultPresentEmojiMap,
-  highlightedEmojiMap: defaultHighlightedEmojiMap,
-  wildcardEmojiMap: defaultWildcardEmojiMap
-});
-
-import viviPresentEmojiMap from "../../assets/emoji-maps/highlighters/vivi/present";
-import viviHighlightedEmojiMap from "../../assets/emoji-maps/highlighters/vivi/highlighted";
-import viviWildcardEmojiMap from "../../assets/emoji-maps/highlighters/vivi/wildcard";
-import { getHighlightedLetters, setHighlightGroups } from "../regex";
-/** This highlighter must be used when using Vivi from an installed application */
-const ViviHighlighter = new EmojiHighlighter({
-  presentEmojiMap: viviPresentEmojiMap,
-  highlightedEmojiMap: viviHighlightedEmojiMap,
-  wildcardEmojiMap: viviWildcardEmojiMap
-});
-
 export const Highlighters = {
   // Themes for OMG
-  Default: DefaultHighlighter,
+  Default: new Highlighter(DefaultCharacterTheme),
+  Caffeinated: new Highlighter(CaffeinatedCharacterTheme),
+  ComicSans: new Highlighter(ComicSansCharacterTheme),
+  Dark: new Highlighter(DarkCharacterTheme),
+  Green: new Highlighter(GreenCharacterTheme),
+  Lifeboat: new Highlighter(LifeboatCharacterTheme),
+  Lilac: new Highlighter(LilacCharacterTheme),
+  Rose: new Highlighter(RoseCharacterTheme),
 
   // Highlighters for installed applications
-  Vivi: ViviHighlighter,
+  Vivi: new Highlighter(ViviCharacterTheme),
 
   // Highlighters for testing
-  Text: new TextHighlighter()
+  Text: new Highlighter(TextCharacterTheme)
 }
