73b743245a0c6d8653b561b1f92944d316684868
iminlikewithyou <christian@omg.games>
2025-06-08 20:13:46 -0500

    fix roblox command bloxlink api is being weird

--- lame-bot/commands/roblox.ts
+++ lame-bot/commands/roblox.ts
@@ -1,62 +1,113 @@
-import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import {
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
 import { replyToInteraction } from "../../src/command-handler";
 import { escapeDiscordMarkdown } from "../../src/utils";
 
 export const data = new SlashCommandBuilder()
   .setName("roblox")
   .setDescription("View a user's Roblox profile!")
-  .addUserOption(option =>
-    option.setName("user")
+  .addUserOption((option) =>
+    option
+      .setName("user")
       .setDescription("The user to view the profile of")
-      .setRequired(false)); 
+      .setRequired(false)
+  );
 
 export const cooldown = 10 * 1000;
 export const broadcastable = true;
 
-export async function execute(interaction: ChatInputCommandInteraction, preferBroadcast: boolean) {
+export async function execute(
+  interaction: ChatInputCommandInteraction,
+  preferBroadcast: boolean
+) {
   const userID = interaction.options.getUser("user")?.id ?? interaction.user.id;
   try {
-    const result = await fetch(`https://api.blox.link/v4/public/guilds/476593983485902850/discord-to-roblox/${userID}`, { headers: { "Authorization": process.env.BLOXLINK_KEY } });
+    const result = await fetch(
+      `https://api.blox.link/v4/public/guilds/476593983485902850/discord-to-roblox/${userID}`,
+      { headers: { Authorization: process.env.BLOXLINK_KEY } }
+    );
     const data = await result.json();
     if (data.error) {
-      const errorMessage = data.error.endsWith(".") ? data.error : data.error + ".";
-      await replyToInteraction(interaction, "Profile", "\n• " + escapeDiscordMarkdown(errorMessage), preferBroadcast);
+      const errorMessage = data.error.endsWith(".")
+        ? data.error
+        : data.error + ".";
+      await replyToInteraction(
+        interaction,
+        "Profile",
+        "\n• " + escapeDiscordMarkdown(errorMessage),
+        preferBroadcast
+      );
       return;
     }
 
     const robloxInformation = data.resolved.roblox;
 
-    const robloxName = robloxInformation.displayName ?? robloxInformation.name;
-    const robloxUsername = robloxInformation.name;
-    const robloxLink = robloxInformation.profileLink;
-    const robloxDescription = robloxInformation.description;
-    
-    // oh my goodness
-    let robloxImage = null;
-    if (robloxInformation.avatar) {
-      if (robloxInformation.avatar.fullBody) {
-        const imageData = await (await fetch(robloxInformation.avatar.fullBody)).json();
-        if (imageData.data && imageData.data[0] && imageData.data[0].imageUrl) {
-          robloxImage = imageData.data[0].imageUrl;
+    if (robloxInformation) {
+      const robloxName =
+        robloxInformation.displayName ?? robloxInformation.name;
+      const robloxUsername = robloxInformation.name;
+      const robloxLink = robloxInformation.profileLink;
+      const robloxDescription = robloxInformation.description;
+
+      // oh my goodness
+      let robloxImage = null;
+      if (robloxInformation.avatar) {
+        if (robloxInformation.avatar.fullBody) {
+          const imageData = await (
+            await fetch(robloxInformation.avatar.fullBody)
+          ).json();
+          if (
+            imageData.data &&
+            imageData.data[0] &&
+            imageData.data[0].imageUrl
+          ) {
+            robloxImage = imageData.data[0].imageUrl;
+          }
         }
       }
-    }
 
-    const text = `<@${userID}> is **${robloxName}** on Roblox.`;
-    const embed = new EmbedBuilder()
-      .setAuthor({ name: "@" + robloxUsername })
-      .setTitle(robloxName)
-      .setURL(robloxLink)
-      .setDescription(robloxDescription)
-      .setThumbnail(robloxImage)
-      .setColor("#00a8ff");
-    
-    await replyToInteraction(interaction, "Profile", "\n" + text + "\n** **", preferBroadcast, {
-      allowedMentions: { parse: [] },
-      embeds: [embed]
-    });
+      const text = `<@${userID}> is **${robloxName}** on Roblox.`;
+      const embed = new EmbedBuilder()
+        .setAuthor({ name: "@" + robloxUsername })
+        .setTitle(robloxName)
+        .setURL(robloxLink)
+        .setDescription(robloxDescription)
+        .setThumbnail(robloxImage)
+        .setColor("#00a8ff");
+
+      await replyToInteraction(
+        interaction,
+        "Profile",
+        "\n" + text + "\n** **",
+        preferBroadcast,
+        {
+          allowedMentions: { parse: [] },
+          embeds: [embed],
+        }
+      );
+    } else {
+      const text = `<@${userID}> is on Roblox. I'm unable to retrieve the profile, so you'll need to [visit their profile link](https://www.roblox.com/users/${data.robloxID}/profile).`;
+
+      await replyToInteraction(
+        interaction,
+        "Profile",
+        "\n" + text,
+        preferBroadcast,
+        {
+          allowedMentions: { parse: [] },
+        }
+      );
+    }
   } catch (error) {
     console.error(error);
-    await replyToInteraction(interaction, "Profile", "\n• An error occurred while fetching the profile.", false);
+    await replyToInteraction(
+      interaction,
+      "Profile",
+      "\n• An error occurred while fetching the profile.",
+      false
+    );
   }
 }
